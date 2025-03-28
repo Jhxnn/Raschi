7b9122fec302ea1bd810d829e87d208d7a4366c4
Hilixx <jhonalizan@gmail.com>
2025-03-28 12:59:22 -0300

    iniciando scrapping

---
+++ src/main/java/com/Raschi/dtos/SearchCarDto.java
@@ -0,0 +1,6 @@
+package com.Raschi.dtos;
+
+import java.util.UUID;
+
+public record SearchCarDto(String brand, String model, String location) {
+}
---
+++ src/main/java/com/Raschi/services/WebScrappingService.java
@@ -0,0 +1,29 @@
+package com.Raschi.services;
+
+
+import com.Raschi.dtos.SearchCarDto;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebScrappingService {
+
+    @Autowired
+    CarService carService;
+
+    @Autowired
+    SellerService sellerService;
+
+    @Autowired
+    SiteService siteService;
+
+    private void findByBrandAndModel(SearchCarDto searchCarDto){
+        String webMotorsUrl = "https://www.webmotors.com.br/carros/estoque/" + searchCarDto.brand() + "/" + searchCarDto.model();
+//      Document webMotors = Jsoup.connect(webMotorsUrl).get();
+        String marketPlaceUrl = "https://www.facebook.com/marketplace/"+ searchCarDto.location()+"/search/?query="+searchCarDto.brand() + "%20"+ searchCarDto.model()+ "&exact=false&locale=pt_BR";
+    }
+
+
+}
