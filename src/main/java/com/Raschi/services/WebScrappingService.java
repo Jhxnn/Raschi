e2a7a1038be656cba19abe38ec67c9a70dff19a5
Hilixx <jhonalizan@gmail.com>
2025-03-28 13:03:28 -0300

    olx url

--- src/main/java/com/Raschi/services/WebScrappingService.java
+++ src/main/java/com/Raschi/services/WebScrappingService.java
@@ -21,8 +21,9 @@ public class WebScrappingService {
 
     private void findByBrandAndModel(SearchCarDto searchCarDto){
         String webMotorsUrl = "https://www.webmotors.com.br/carros/estoque/" + searchCarDto.brand() + "/" + searchCarDto.model();
-//      Document webMotors = Jsoup.connect(webMotorsUrl).get();
         String marketPlaceUrl = "https://www.facebook.com/marketplace/"+ searchCarDto.location()+"/search/?query="+searchCarDto.brand() + "%20"+ searchCarDto.model()+ "&exact=false&locale=pt_BR";
+        String olxUrl = "https://www.olx.com.br/estado-sc?q="+ searchCarDto.brand()+ "%20"+ searchCarDto.model();
+//      Document webMotors = Jsoup.connect(webMotorsUrl).get();
     }
 
 
